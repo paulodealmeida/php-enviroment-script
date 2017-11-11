287933e84569711bd46c7a44bf9684ce1293d44b
Paulo de Almeida <paulodealmeida@gmail.com>
2017-11-11 08:26:13 -0300

    Adicionada instalação do docker-compose

--- ubuntu.sh
+++ ubuntu.sh
@@ -59,6 +59,10 @@ fancy_echo "Instalando docker ..."
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update
   sudo apt-get install -y docker-ce
+  
+fancy_echo "Instalando docker ..."
+  sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 
 fancy_echo "Instalando aplicativos comuns ..."
   sudo apt-get install -y unrar unzip vim
