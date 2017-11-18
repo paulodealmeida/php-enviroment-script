219416a07c058607336e65c321edfa9ffda423f4
Paulo de Almeida <paulodealmeida@gmail.com>
2017-11-18 09:18:47 -0300

    Instalação do spotify com confirmação

--- ubuntu.sh
+++ ubuntu.sh
@@ -71,7 +71,7 @@ fancy_echo "Instalando Spotify ..."
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
   echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
   sudo apt-get update
-  sudo apt-get install spotify-client
+  sudo apt-get install -y spotify-client
 
 fancy_echo "Instalando Oracle JDK 8 ..."
   sudo apt-get install -y default-jre
