3bb97c68dfe99e816ce18c29c7e15a5954d1e10a
Paulo de Almeida <paulodealmeida@gmail.com>
2018-03-01 13:29:07 -0300

    Alterada instalação do sublime
    Alterada a instalação do sublime para instalar a versão stable.

--- ubuntu.sh
+++ ubuntu.sh
@@ -34,14 +34,14 @@ fancy_echo "Verificando sistema ..."
   check_sistem
 
 fancy_echo "Atualizando sistema ..."
-  sudo apt-get update
+  sudo apt update
 
 fancy_echo "Instalando Sublime Text ..."
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-  sudo apt-get install -y apt-transport-https
-  echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-  sudo apt-get update
-  sudo apt-get install -y sublime-text
+  sudo apt install -y apt-transport-https
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  sudo apt update
+  sudo apt install -y sublime-text
 
 fancy_echo "Instalando git ..."
   sudo apt-get install -y git
